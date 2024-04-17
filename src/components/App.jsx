e9b631c1020a32546560b8112fc1dd863523be4f
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-04-17 12:33:14 +0200

    update local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
+import React, { useState } from 'react';
+import { ContactForm } from 'components/ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filtrer';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  // dodawanie kontaktu i sprawdzanie, czy taki juz istnieje w ksiązce
+  const addContact = contact => {
+    const isInPhonebook = contacts.some(
+      phoneContact =>
+        phoneContact.name.toLowerCase() === contact.name.toLowerCase()
+    );
+    if (isInPhonebook) {
+      alert(`${contact.name} is already in contacts.`);
+    } else {
+      setContacts([...contacts, contact]);
+    }
+  };
+  // usuwanie z listy
+  const deleteContact = id => {
+    const updatedLists = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedLists);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter
+        value={filter}
+        onChange={event => setFilter(event.target.value)}
+      />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        deleteContact={deleteContact}
+      />
     </div>
   );
 };
---
+++ src/components/localStorage.jsx
@@ -0,0 +1,5 @@
+// import { useState, useEffect } from "react";
+
+// const getStorageValue (key, defaultValue) {
+//     const saved
+// }
